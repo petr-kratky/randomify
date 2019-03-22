7d2b5818710ce30db3e67a8edad8bb7fc8b8ee24
Petr Kratky <kratky.pete@gmail.com>
2019-03-22 23:54:48 +0100

    move auth logic to App.js

--- App.js
+++ App.js
@@ -2,16 +2,114 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import AlbumDetail from './src/components/AlbumDetail';
 import RandomButton from "./src/components/RandomButton";
-import { Font, LinearGradient } from "expo";
+import {AuthSession, Font, LinearGradient} from "expo";
+import { encode as btoa } from 'base-64';
 
 export default class App extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+
+        super(props);
 
         this.state = {
-            fontsLoaded: false
+            fontsLoaded: false,
+            login: false,
+            tokens: {
+                accessToken: 'no_access_token', // access token required by api calls
+                refreshToken: 'no_refresh_token', // token to refresh (obtain new) access token wo user interference
+                expiresIn: 'no_expire_time' // expiry time for the access token (6 minutes)
+            }
         }
     }
+
+
+    /*
+    @getAuthorizationCode:
+        requests auth code from spotify api using application credentials
+        launches auth session for the user to grant randomify access to their account
+        returns @result.params.code (auth_code from the session)
+
+        @redirectUrl: url where user will be redirected after session is complete
+        @clientID: randomify application ID
+        @scopes: list of rights the user grants to the app; none are required for search api endpoint
+    */
+
+    // TODO: securely store login credentials on backend and request them via API call
+
+    getAuthorizationCode = async () => {
+        try {
+            let result;
+            const redirectUrl = AuthSession.getRedirectUrl();
+            const clientID = '8a352836ac464579ab2e790ee597a703';
+            const scopes = ''; //
+            result = await AuthSession.startAsync({
+                behavior: 'web',
+                authUrl:
+                    'https://accounts.spotify.com/authorize' +
+                    '?response_type=code' +
+                    '&client_id=' +
+                    clientID +
+                    (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+                    '&redirect_uri=' +
+                    encodeURIComponent(redirectUrl),
+            });
+            return result.params.code;
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    /*
+    @getTokens:
+        uses auth_code from @getAuthorizationCode() to request access_token required by api calls
+        stores obtained tokens (access_token, refresh_token) and access token's expire time in 'tokens' state
+
+        @authorizationCode: auth_code from auth session
+        @credentials: randomify's application credentials
+        @credsB64: base64-encoded string with credentials
+        @responseJson: response from the request stored in .json format
+    */
+
+    // TODO: use refresh_token to obtain new access_token automatically once expired
+
+    getTokens = async () => {
+      try {
+        const authorizationCode = await this.getAuthorizationCode();
+        const credentials = {
+            clientId: '8a352836ac464579ab2e790ee597a703',
+            clientSecret: 'ae0d353236ff4dd780b32670fb55f97a',
+            redirectUri: AuthSession.getRedirectUrl()
+        };
+        const credsB64 = btoa(`${credentials.clientId}:${credentials.clientSecret}`);
+        const response = await fetch('https://accounts.spotify.com/api/token', {
+          method: 'POST',
+          headers: {
+            Authorization: `Basic ${credsB64}`,
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: `grant_type=authorization_code&code=${authorizationCode}&redirect_uri=${
+            credentials.redirectUri
+          }`,
+        });
+        const responseJson = await response.json();
+
+        const {
+          access_token: accessToken,
+          refresh_token: refreshToken,
+          expires_in: expiresIn,
+        } = responseJson;
+
+        this.setState({
+            tokens: {
+                accessToken: accessToken,
+                refreshToken: refreshToken,
+                expiresIn: expiresIn
+            }});
+
+
+      } catch (err) {
+        console.error(err);
+      }
+    };
 
     /*
     @loadFonts:
@@ -25,7 +123,12 @@ export default class App extends Component {
 
     componentDidMount() {
         this.loadFonts();
+        this.getTokens()
+            .then(() => {
+                this.setState({login: true});
+            })
     }
+
 
     render() {
         if(!this.state.fontsLoaded) {
@@ -48,7 +151,10 @@ export default class App extends Component {
                     start={[0.4, 0.1]}
                     style={{flex: 1}}
                 >
-                <AlbumDetail ref={(AlbumDetail) => {this.AlbumDetail = AlbumDetail;} } />
+                <AlbumDetail
+                    ref={(AlbumDetail) => {this.AlbumDetail = AlbumDetail;}}
+                    accessToken={this.state.tokens.accessToken}
+                />
                 <RandomButton whenPressed={() => {this.AlbumDetail.refreshAlbum()}}>
                     <Text style={randomButtonTextStyle}>RANDOMIFY</Text>
                 </RandomButton>
--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,8 +2,7 @@ import React, {Component} from 'react';
 import {Image, Linking, Text, View} from 'react-native';
 
 import Spotify from 'spotify-web-api-js';
-import { AuthSession, Font } from 'expo';
-import { encode as btoa } from 'base-64'
+import { Font } from 'expo';
 
 import Card from './Card';
 import CardSection from './CardSection';
@@ -17,11 +16,6 @@ export default class AlbumDetail extends Component {
 
         this.state = {
             fontsLoaded: false,
-            tokens: {
-                accessToken: 'no_access_token', // access token required by api calls
-                refreshToken: 'no_refresh_token', // token to refresh (obtain new) access token wo user interference
-                expiresIn: 'no_expire_time' // expiry time for the access token (6 minutes)
-            },
             album: {
                 id: 'album_id',
                 name: 'album_name',
@@ -35,94 +29,6 @@ export default class AlbumDetail extends Component {
             }
         };
     }
-
-    /*
-    @getAuthorizationCode:
-        requests auth code from spotify api using application credentials
-        launches auth session for the user to grant randomify access to their account
-        returns @result.params.code (auth_code from the session)
-
-        @redirectUrl: url where user will be redirected after session is complete
-        @clientID: randomify application ID
-        @scopes: list of rights the user grants to the app; none are required for search api endpoint
-    */
-
-    // TODO: securely store login credentials on backend and request them via API call
-
-    getAuthorizationCode = async () => {
-        try {
-            let result;
-            const redirectUrl = AuthSession.getRedirectUrl();
-            const clientID = '8a352836ac464579ab2e790ee597a703';
-            const scopes = ''; //
-            result = await AuthSession.startAsync({
-                behavior: 'web',
-                authUrl:
-                    'https://accounts.spotify.com/authorize' +
-                    '?response_type=code' +
-                    '&client_id=' +
-                    clientID +
-                    (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-                    '&redirect_uri=' +
-                    encodeURIComponent(redirectUrl),
-            });
-            return result.params.code;
-        } catch (err) {
-            console.error(err);
-        }
-    };
-
-    /*
-    @getTokens:
-        uses auth_code from @getAuthorizationCode() to request access_token required by api calls
-        stores obtained tokens (access_token, refresh_token) and access token's expire time in 'tokens' state
-
-        @authorizationCode: auth_code from auth session
-        @credentials: randomify's application credentials
-        @credsB64: base64-encoded string with credentials
-        @responseJson: response from the request stored in .json format
-    */
-
-    // TODO: use refresh_token to obtain new access_token automatically once expired
-
-    getTokens = async () => {
-      try {
-        const authorizationCode = await this.getAuthorizationCode();
-
-        const credentials = {
-            clientId: '8a352836ac464579ab2e790ee597a703',
-            clientSecret: 'ae0d353236ff4dd780b32670fb55f97a',
-            redirectUri: AuthSession.getRedirectUrl()
-        };
-        const credsB64 = btoa(`${credentials.clientId}:${credentials.clientSecret}`);
-        const response = await fetch('https://accounts.spotify.com/api/token', {
-          method: 'POST',
-          headers: {
-            Authorization: `Basic ${credsB64}`,
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-          body: `grant_type=authorization_code&code=${authorizationCode}&redirect_uri=${
-            credentials.redirectUri
-          }`,
-        });
-        const responseJson = await response.json();
-
-        const {
-          access_token: accessToken,
-          refresh_token: refreshToken,
-          expires_in: expiresIn,
-        } = responseJson;
-
-        this.setState({tokens: {
-                accessToken: accessToken,
-                refreshToken: refreshToken,
-                expiresIn: expiresIn
-            }});
-
-      } catch (err) {
-        console.error(err);
-      }
-    };
 
     /*
     @randomNum:
@@ -237,12 +143,15 @@ export default class AlbumDetail extends Component {
 
     componentDidMount() {
         this.loadFonts();
+    }
 
-        this.getTokens()
-            .then(() => {
-                sp.setAccessToken(this.state.tokens.accessToken);
-                this.refreshAlbum();
-            })
+    componentDidUpdate(prevProps) {
+        if (this.props.accessToken !== prevProps.accessToken) {
+            console.log('AlbumDetail accessToken: ' + this.props.accessToken);
+            sp.setAccessToken(this.props.accessToken);
+            this.refreshAlbum()
+        }
+
     }
 
     render() {
